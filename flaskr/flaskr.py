1211189583e272ef67815e1ebf99da8be0590733
alexsanjoseph <alexsanjoseph@gmail.com>
2017-02-18 11:41:23 +0530

    4.0

--- flaskr/flaskr.py
+++ flaskr/flaskr.py
@@ -21,6 +21,18 @@ def connect_db():
     rv.row_factory = sqlite3.Row
     return rv
 
-dbcon = connect_db()
+def get_db():
+    """Opens a new database connection if there is none yet for the
+    current application context.
+    """
+    if not hasattr(g, 'sqlite_db'):
+        g.sqlite_db = connect_db()
+    return g.sqlite_db
 
-sqlite3.
+@app.teardown_appcontext
+def close_db(error):
+    """Closes the database again at the end of the request."""
+    if hasattr(g, 'sqlite_db'):
+        g.sqlite_db.close()
+        
+dbcon = connect_db()
