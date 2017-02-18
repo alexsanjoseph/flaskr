636a8ad1f7e713e1cf6d8358d6cc3775a42d6a19
alexsanjoseph <alexsanjoseph@gmail.com>
2017-02-18 12:22:52 +0530

    5.0

--- flaskr/flaskr.py
+++ flaskr/flaskr.py
@@ -29,10 +29,22 @@ def get_db():
         g.sqlite_db = connect_db()
     return g.sqlite_db
 
+def init_db():
+    db = get_db()
+    with app.open_resource('schema.sql', mode='r') as f:
+        db.cursor().executescript(f.read())
+    db.commit()
+
+@app.cli.command('initdb')
+def initdb_command():
+    """Initializes the database."""
+    init_db()
+    print('Initialized the database.')
+
 @app.teardown_appcontext
 def close_db(error):
     """Closes the database again at the end of the request."""
     if hasattr(g, 'sqlite_db'):
         g.sqlite_db.close()
-        
+
 dbcon = connect_db()
